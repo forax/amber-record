daf8924f32052a25c336124f4262307b11c739e0
forax <forax@localhost>
2019-12-21 22:01:44 +0100

    add a non-sealed class

---
+++ src/main/java/fr.umlv.record/fr/umlv/sealed/NonSealedExample.java
@@ -0,0 +1,17 @@
+package fr.umlv.sealed;
+
+import java.util.Arrays;
+
+public class NonSealedExample {
+  sealed interface I {
+  }
+  non-sealed static class A implements I {
+  }
+  
+  public static void main(String[] args) {
+		I i = new A();
+  	
+		var permitted = I.class.getPermittedSubtypes();
+		System.out.println(Arrays.toString(permitted));
+	}
+}
