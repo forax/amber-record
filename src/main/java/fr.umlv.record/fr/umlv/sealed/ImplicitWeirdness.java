3890727c1cd15f708745024b255a0958b9247faf
forax <forax@univ-mlv.fr>
2020-03-17 16:53:21 +0100

    move to package fr.umlv.sealed

--- src/main/java/fr.umlv.record/fr/umlv/sealed/ImplicitWeirdness.java
+++ src/main/java/fr.umlv.record/fr/umlv/sealed/ImplicitWeirdness.java
@@ -1,9 +1,9 @@
-package fr.umlv.record;
+package fr.umlv.sealed;
 
-/*
+
 public class ImplicitWeirdness {
   sealed interface I { }
   sealed interface J extends I { }
   final static class A implements I, J { }
 }
-*/
+
